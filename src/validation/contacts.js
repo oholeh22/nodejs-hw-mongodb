765244e7c619a28ba71e70d53d566e6555edf619
Oleh Shyian <shiyan4242@gmail.com>
2025-02-03 21:32:58 +0100

    fix

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -3,11 +3,11 @@ import Joi from 'joi';
 export const contactCreateSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   email: Joi.string().email().min(3).max(20).required(),
-  phone: Joi.string().min(3).max(20).required(),
+  phoneNumber: Joi.string().min(3).max(20).required(),
 });
 
 export const contactUpdateSchema = Joi.object({
   name: Joi.string().min(3).max(20),
   email: Joi.string().email().min(3).max(20),
-  phone: Joi.string().min(3).max(20),
-}).min(1); 
+  phoneNumber: Joi.string().min(3).max(20),
+}).min(1);
