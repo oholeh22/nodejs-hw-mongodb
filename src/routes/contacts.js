b22e14250880165f8acc5b2d6105f5643728b751
Oleh Shyian <shiyan4242@gmail.com>
2025-02-14 11:59:26 +0100

    fix

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -1,19 +1,21 @@
 import { Router } from "express";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 import {
-    getContactsController,
-    getContactByIdController,
-    createContactController,
-    deleteContactController,
-    patchContactController,
-    upsertContactController,
-  } from '../controllers/contacts.js';
+  getContactsController,
+  getContactByIdController,
+  createContactController,
+  deleteContactController,
+  patchContactController,
+  upsertContactController,
+} from '../controllers/contacts.js';
 import { contactCreateSchema, contactUpdateSchema } from "../validation/contacts.js";
 import { isValidId } from "../middlewares/isValidId.js";
 import { validateBody } from "../middlewares/validateBody.js";
-
+import { authenticate } from '../middlewares/authenticate.js';
 
 const router = Router();
+
+router.use(authenticate);
 
 router.get('/', ctrlWrapper(getContactsController));
 
@@ -26,6 +28,5 @@ router.delete('/:contactId', isValidId, ctrlWrapper(deleteContactController));
 router.put('/:contactId', isValidId, ctrlWrapper(upsertContactController));
 
 router.patch('/:contactId', isValidId, validateBody(contactUpdateSchema), ctrlWrapper(patchContactController));
-
 
 export default router;
