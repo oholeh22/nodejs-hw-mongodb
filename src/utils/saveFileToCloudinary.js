9bd3614954916b79b090b2573d996e58d140e245
Oleh Shyian <shiyan4242@gmail.com>
2025-02-23 17:21:05 +0100

    fix

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -2,7 +2,7 @@ import cloudinary from 'cloudinary';
 import fs from 'node:fs/promises';
 
 import { getEnvVar } from './getEnvVar.js';
-import { CLOUDINARY } from '../constants/contacts.js';
+import { CLOUDINARY } from '../constans/index.js';
 
 cloudinary.v2.config({
   secure: true,
